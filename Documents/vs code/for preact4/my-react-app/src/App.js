574874bb5ecd11653e33388cd1d0a3f2eeb7dd31
n0orw <noorulhuq789@gmail.com>
2025-02-02 20:01:17 +0530

    adding my portfolio

---
+++ Documents/vs code/for preact4/my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header(){
+  return <h1>hello sffexyyworold mttother flickerszzrzz</h1>
+
+}
+
+function App() {
+return <Header />;
+}
+
+export default App;
